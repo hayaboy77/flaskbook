4cb1eab992df74d6d208263475c4174d2731a2f4
hayaboy77 <hayaboy77@gmail.com>
2025-12-23 17:03:10 +0900

    flask_2

--- helloFlask.py
+++ helloFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 
 
@@ -6,17 +6,36 @@ print(f'__name__ : {__name__}' )
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/',  endpoint='root')
 def index():
     return "<p>안녕</p>"
 
 
-@app.route('/trans')
-def trans():
-    return "<p>교통현황 대시보드</p>"
+@app.route('/tomorrow/<menu>')
+def tomorrow(menu):
+    return render_template('index.html', menu=menu)
+
+
+# @app.route('/trans1')
+@app.get('/trans')
+def transGet():
+    return "<p>교통현황 대시보드-GET방식</p>"
+
+
+@app.post('/trans')
+def transPost():
+    return "<p>교통현황 대시보드-POST방식</p>"
 
 
 @app.route('/hello')
 def hello():
-    name = request.args.get('name', 'Christmas')
+    name = request.args.get('name2', 'Christmas')
     return f"<p>안녕 {name}</p>"
+
+
+# @app.route 데코레이터의 Rule에 변수를 지정할 수 있다. 변수는 <변수명>
+@app.route('/hello2/<height>')
+def hello2(height):    
+    height=int(height)
+    result=height+7
+    return f"<p>안녕 {result}</p>"
